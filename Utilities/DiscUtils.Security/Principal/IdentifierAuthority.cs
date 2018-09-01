e87c03b965f903d03f55baad07287108fa4302dd
Michael Bikovitsky <moshe.biko@gmail.com>
2018-09-01 21:06:53 +0300

    Add SecurityIdentifier and related classes

---
+++ Utilities/DiscUtils.Security/Principal/IdentifierAuthority.cs
@@ -0,0 +1,19 @@
+﻿namespace DiscUtils.Security.Principal
+{
+    //
+    // Identifier authorities
+    //
+    internal enum IdentifierAuthority : long
+    {
+        NullAuthority = 0,
+        WorldAuthority = 1,
+        LocalAuthority = 2,
+        CreatorAuthority = 3,
+        NonUniqueAuthority = 4,
+        NTAuthority = 5,
+        SiteServerAuthority = 6,
+        InternetSiteAuthority = 7,
+        ExchangeAuthority = 8,
+        ResourceManagerAuthority = 9,
+    }
+}
